341d4a34345f83af05222d3319f7171b76e3b6ae
aMenuVerse Developer <developer@amenuverse.com>
2026-08-25 10:14:16 +0600

    refactor: update PM2 ecosystem script entrypoint to server.js

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   apps: [
     {
       name: "amenuverse",
-      script: ".output/server/index.mjs",
+      script: "server.js",
       instances: 1,
       exec_mode: "fork",
       watch: false,
